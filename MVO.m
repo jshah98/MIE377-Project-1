98937465719ed3446e82fe82a59e9356ea4cc7aa
JoshChang98 <47726243+JoshChang98@users.noreply.github.com>
2019-02-18 21:31:03 -0500

    Update MVO.m

--- MVO.m
+++ MVO.m
@@ -10,8 +10,10 @@ function  x = MVO(mu, Q, targetRet, varargin)
     % Find the total number of assets
     n = size(Q,1);
     f = zeros(n, 1);
-
+    
+    % Increasing the tolerance of 'quadprog'
+    options = optimoptions( 'quadprog', 'TolFun', 1e-9 );
     % quadprog (below) minimizes 0.5*x' * Q *x s.t. -1*mu*x <= targetRet, [col of ones] * x = 1, and 0 <= xi <= 1
 
-    x = quadprog(Q, f, -1*mu, targetRet, ones(1, n), 1, zeros(1,n), ones(1,n))
+    x = quadprog(Q, f, -1*mu, targetRet, ones(1, n), 1, zeros(1,n), ones(1,n),[],options)
 end
